a164daf9db409acfd6642606ce7b5f69624663e2
armstrob <bruce.armstrong88@gmail.com>
2017-07-07 21:11:35 -0400

    try it from home

--- www/js/index.js
+++ www/js/index.js
@@ -18,26 +18,33 @@
  */
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+    onDeviceReady: function () {
         app.receivedEvent('deviceready');
+        var mw_c = mwbScanner.getConstants(), settings;
+        settings = [{ 'method': 'MWBsetActiveCodes', 'value': [mw_c.MWB_CODE_MASK_39 | mw_c.MWB_CODE_MASK_DM] }];
+        mwbScanner.loadSettings(settings).then(function (response) {
+            console.log(response);
+        }).catch(function (reason) {
+            console.log(reason);
+        });
         scannerConfig();
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent: function (id) {
 
         console.log('Received Event: ' + id);
     }
